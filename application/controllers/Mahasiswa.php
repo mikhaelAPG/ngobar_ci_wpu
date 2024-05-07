ea3780aecfb66f7b9ef6e3a720e7d1b392140587
MikhaelAdrielPratamaGana <mikhael.adriel.pratama.gana@gmail.com>
2024-05-07 18:16:37 +0700

    bagian 2

--- application/controllers/Mahasiswa.php
+++ application/controllers/Mahasiswa.php
@@ -4,6 +4,7 @@ class Mahasiswa extends CI_Controller {
 	public function __construct() {
 		parent::__construct();
 		$this->load->model('Mahasiswa_model');
+		$this->load->library('form_validation');
 	}
 
 	public function index()
@@ -13,5 +14,31 @@ class Mahasiswa extends CI_Controller {
 		$this->load->view('template/header', $data);
 		$this->load->view('mahasiswa/index', $data);
 		$this->load->view('template/footer');
+	}
+
+	public function tambah()
+	{
+		$data['judul'] = 'Form Tambah Data Mahasiswa';
+
+		$this->form_validation->set_rules('nama', 'Nama', 'required');
+		$this->form_validation->set_rules('nrp', 'NRP', 'required|numeric');
+		$this->form_validation->set_rules('email', 'Email', 'required|valid_email');
+
+		if ($this->form_validation->run() == FALSE) {
+			$this->load->view('template/header', $data);
+			$this->load->view('mahasiswa/tambah');
+			$this->load->view('template/footer');
+		} else {
+			$this->Mahasiswa_model->tambahDataMahasiswa();
+			$this->session->set_flashdata('flash', 'Ditambahkan');
+			redirect('mahasiswa');
+		}
+	}
+
+	public function hapus($id)
+	{
+		$this->Mahasiswa_model->hapusDataMahasiswa($id);
+		$this->session->set_flashdata('flash', 'Dihapus');
+		redirect('mahasiswa');
 	}
 }
